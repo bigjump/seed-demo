d602c37e2122a92ca6fda4c215f296376856ab55
Jay McGuinness <jay@bigjump.co.uk>
2020-05-07 22:04:05 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,5 +7,6 @@ then
   apt -qq -y install zip
 fi
 
+dotnet tool install -g Amazon.Lambda.Tools
 dotnet restore
 dotnet lambda package --configuration release --framework netcoreapp3.1 --output-package bin/release/netcoreapp3.1/hello.zip
